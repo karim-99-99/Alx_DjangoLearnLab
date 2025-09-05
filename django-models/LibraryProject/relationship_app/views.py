fc9da14fda9043d72eb734597f9a8ddfbf906ba7
karim-99-99 <kareem.hossam99@yahoo.com>
2025-09-05 21:07:03 +0300

    advanced django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book , Library
 
 # Create your views here.
+
+def list_books(request):
+    books = Book.object.select_related('author').all()
+    output = "<h2>Books and their Authors</h2><ul>"
+    for book in books:
+        output += f"<li>{book.title} by {book.author.name}</li>"
+    output += "</ul>"
+    return HttpResponse(output)
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship/library_detail.html'
+    context_object_name = 'library'
+    
