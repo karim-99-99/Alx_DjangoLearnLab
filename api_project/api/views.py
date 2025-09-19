3c1b2b6762bfb649b1a8ef1143366956da6afe90
karim-99-99 <kareem.hossam99@yahoo.com>
2025-09-19 19:04:38 +0300

    final edite

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -4,6 +4,6 @@ from .serializers import BookSerializer
 from Alx_DjangoLearnLab.api_project.api.serializers import BookSerializer
 from .models import Book
 # Create your views here.
-class BookList(generics.ListCreateAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
