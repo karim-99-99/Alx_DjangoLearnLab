88bb26e7ec4501f0449a21d1666061b5bb447954
karim-99-99 <kareem.hossam99@yahoo.com>
2025-09-19 19:03:43 +0300

    final edite

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics
-
+from .serializers import BookSerializer
 from Alx_DjangoLearnLab.api_project.api.serializers import BookSerializer
 from .models import Book
 # Create your views here.
