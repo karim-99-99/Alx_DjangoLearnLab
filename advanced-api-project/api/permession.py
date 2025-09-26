f519b0a753f71ff4498f28a4344d8a70ec10d323
karim-99-99 <kareem.hossam99@yahoo.com>
2025-09-26 21:45:59 +0300

    first commit

--- advanced-api-project/api/permession.py
+++ advanced-api-project/api/permession.py
@@ -1,5 +1,5 @@
 from rest_framework import permissions
-from rest_framework.exceptions import PermissionDenied
+from rest_framework.permissions import IsAuthenticatedOrReadOnly ,IsAuthenticated 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
