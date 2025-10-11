ed2e4d68e84186606c74963b00ba9dbe0cef1aac
karim-99-99 <kareem.hossam99@yahoo.com>
2025-10-11 14:10:40 +0300

    hello

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -18,7 +18,7 @@ class Post(models.Model):
         return reverse('post_detail' , kwargs={'pk' : self.pk})
 
 
-class Comment (models.Model):
+class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete= models.CASCADE, related_name='comments')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
     content = models.TextField()
