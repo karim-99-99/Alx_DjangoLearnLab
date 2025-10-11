353cfcaefbf115f0c165f76e31e671f8faa0a226
karim-99-99 <kareem.hossam99@yahoo.com>
2025-10-11 11:20:56 +0300

    hello

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -7,11 +7,18 @@ urlpatterns = [
     path('', views.home, name='home'),
 
     # Blog posts
-    path('posts/', views.PostListView.as_view(), name='post-list'),
-    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
-    path('posts/new/', views.PostCreateView.as_view(), name='post-create'),
-    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-update'),
-    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+    # path('posts/', views.PostListView.as_view(), name='post-list'),
+    # path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    # path('posts/new/', views.PostCreateView.as_view(), name='post-create'),
+    # path('posts/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
+    # path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+
+
+   path('post/', views.PostListView.as_view(), name='post-list'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 
     # Authentication
     path('register/', views.register, name='register'),
